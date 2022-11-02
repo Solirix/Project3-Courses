d526f5e72ad1717c611e64945aaa7029720f136a
Solirix <hades5132@gmail.com>
2022-11-02 08:56:29 -0400

    hash table functionality implemented

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
-#include <iostream> 
-#include <string>
+
+#include "hashTable.hpp"
 
 int main(void) {
     int userInput = 0;
 
     std::cin >> userInput;
+    
+    hashTable myTable = hashTable(10);
+    myTable.insert(userInput);
+    myTable.printTable();
 
     return 0;
 }
